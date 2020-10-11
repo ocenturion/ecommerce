01aa3832def52304fead125d779771c90c419079
oscar <centurion.oscar90@gmail.com>
2020-10-11 20:47:34 -0300

    OC - cambios en adminecommerce y abmproductos

--- adminEcommerce.php
+++ adminEcommerce.php
@@ -22,20 +22,35 @@
         <div class="row">
             <div class="col-md-3 bg-secondary text-light">
                 <div class="list-group">
-                    <a href="#" class="list-group-item list-group-item-action active">Cras justo odio</a>
-                    <a href="#" class="list-group-item list-group-item-action">Dapibus ac facilisis in</a>
-                    <a href="#" class="list-group-item list-group-item-action">Morbi leo risus</a>
-                    <a href="#" class="list-group-item list-group-item-action">Porta ac consectetur ac</a>
-                    <a href="#" class="list-group-item list-group-item-action disabled" tabindex="-1" aria-disabled="true">Vestibulum at eros</a>
+                    <a class="list-group-item list-group-item-action" data-toggle="list" id="abmProductos" onclick="mostrarTabla('abmProductos')">ABM Productos</a>
+                    <a class="list-group-item list-group-item-action" data-toggle="list" onclick="mostrarTabla('abmCategorías')">ABM Categorías</a>
+                    <a class="list-group-item list-group-item-action" data-toggle="list" onclick="mostrarTabla('abmProveedores')">ABM Proveedores</a>
+                    <a class="list-group-item list-group-item-action" data-toggle="list" onclick="mostrarTabla('Reportes')">Reportes</a>
+                    <a class="list-group-item list-group-item-action" data-toggle="list" onclick="mostrarTabla('Misdatos')">Mis datos</a>
                 </div>
             </div>
             <div class="col-md-9">
-
+                <?php include("abmProductos.php");?>
             </div>
         </div>
     </div>
     
     <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.1/dist/umd/popper.min.js" integrity="sha384-9/reFTGAW83EW2RDu2S0VKaIzap3H66lZH81PoYlFhbGU+6BZp6G7niu735Sk7lN" crossorigin="anonymous"></script>
     <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js" integrity="sha384-B4gt1jrGC7Jh4AgTPSdUtOBvfO8shuf57BaghqFfPlYxofvL8/KUEfYiJOMMV+rV" crossorigin="anonymous"></script>
+    <script>
+        function mostrarTabla(abm){  
+            if (abm==="abmProductos") {
+                document.getElementById('tituloDelAbm').innerHTML="ABM Productos";
+            }else if (abm==="abmCategorías"){
+                document.getElementById('tituloDelAbm').innerHTML="ABM Categorías";
+            }else if (abm==="abmProveedores"){
+                document.getElementById('tituloDelAbm').innerHTML="ABM Proveedores";
+            }else if (abm==="Reportes"){
+                document.getElementById('tituloDelAbm').innerHTML="Reportes";
+            }else{
+                document.getElementById('tituloDelAbm').innerHTML="Mis datos";
+            }  
+        }
+    </script>
 </body>
 </html>
