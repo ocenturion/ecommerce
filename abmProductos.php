def7f282bc09600d13acf923f27439bef678dc35
oscar <centurion.oscar90@gmail.com>
2020-10-17 00:18:42 -0300

    OC - cambios en el modal de editar.

--- abmProductos.php
+++ abmProductos.php
@@ -1,9 +1,7 @@
 
 <h3 class="text-center mt-4" id="tituloDelAbm"></h3>
 <!-- Button trigger modal -->
-<div id="btnAgregarPadre">
-
-</div>
+<button id="btnAgregar" type="button" onclick="onclick_btnAgregar(btnAgregar)" class="btn btn-primary mb-3" data-toggle="modal" data-target="#exampleModal" style="visibility:hidden;"></button>
 <table class="table table-dark">
   <thead>
     <tr id="tableHead">
@@ -17,8 +15,8 @@
 
 
 
-<!-- Modal -->
-<div class="modal fade" id="exampleModal" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
+<!-- Modal AGREGAR -->
+<div class="modal fade" id="modalAgregar" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
   <div class="modal-dialog">
     <div class="modal-content">
       <div class="modal-header">
@@ -27,12 +25,54 @@
           <span aria-hidden="true">&times;</span>
         </button>
       </div>
-      <div class="modal-body">
+      <div class="modal-body" id="bodyModal">
+        ...
+      </div>
+      <div class="modal-footer">
+        <button type="button" class="btn btn-secondary" data-dismiss="modal">Cerrar</button>
+        <button type="button" class="btn btn-primary">Agregar</button>
+      </div>
+    </div>
+  </div>
+</div>
+
+<!-- Modal MODIFICAR -->
+<div class="modal fade" id="modalModificar" tabindex="-1" aria-labelledby="modificarModalLabel" aria-hidden="true">
+  <div class="modal-dialog modal-lg">
+    <div class="modal-content">
+      <div class="modal-header">
+        <h5 class="modal-title" id="tituloModificar"></h5>
+        <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+          <span aria-hidden="true">&times;</span>
+        </button>
+      </div>
+      <div class="modal-body" id="bodyModificar">
         ...
       </div>
       <div class="modal-footer">
         <button type="button" class="btn btn-secondary" data-dismiss="modal">Cerrar</button>
         <button type="button" class="btn btn-primary">Guardar Cambios</button>
+      </div>
+    </div>
+  </div>
+</div>
+
+<!-- Modal ELIMINAR -->
+<div class="modal fade" id="modalEliminar" tabindex="-1" aria-labelledby="modalEliminarLabel" aria-hidden="true">
+  <div class="modal-dialog">
+    <div class="modal-content">
+      <div class="modal-header">
+        <h5 class="modal-title" id="tituloModalEliminar"></h5>
+        <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+          <span aria-hidden="true">&times;</span>
+        </button>
+      </div>
+      <div class="modal-body" id="bodyModal">
+        ¿Seguro que desea eliminar el registro?
+      </div>
+      <div class="modal-footer">
+        <button type="button" class="btn btn-secondary" data-dismiss="modal">Cancelar</button>
+        <button type="button" class="btn btn-danger">Eliminar</button>
       </div>
     </div>
   </div>
--- adminEcommerce.php
+++ adminEcommerce.php
@@ -18,7 +18,7 @@
 </head>
 <body>
     <?php include("menuAdmin.php")?>
-
+        
     <div class="container-fluid ricardoArjona">
         <div class="row">
             <div class="col-md-3 bg-secondary text-light">
@@ -40,11 +40,12 @@
     <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js" integrity="sha384-B4gt1jrGC7Jh4AgTPSdUtOBvfO8shuf57BaghqFfPlYxofvL8/KUEfYiJOMMV+rV" crossorigin="anonymous"></script>
     <script>
         function mostrarTabla(abm){  
-            /* document.getElementById('btnAgregar').style.visibility="visible"; */
+            document.getElementById('btnAgregar').style.visibility="visible";
             if (abm==="abmProductos") {
                 document.getElementById('tituloDelAbm').innerHTML="ABM Productos";
-                document.getElementById('btnAgregarPadre').innerHTML='<button id="btnAgregar" type="button" onclick="onclick_btnAgregar(btnAgregar)" class="btn btn-primary mb-3" data-toggle="modal" data-target="#exampleModal"></button>';
                 document.getElementById('btnAgregar').innerHTML="Agregar Productos";
+                document.getElementById('tituloModal').innerHTML="Agregar Productos";
+                
                 document.getElementById('tableHead').innerHTML=`
                                                                 <th>Id</th>
                                                                 <th>Nombre</th>
@@ -112,13 +113,13 @@
                                 <td>${element.categoria}</td>
                                 <td>${element.proveedor}</td>
                                 <td>
-                                    <button class="btn btn-secondary" data-toggle="modal" data-target="#exampleModal" onclick="onclick_btnEditar()">
+                                    <button class="btn btn-secondary" data-toggle="modal" data-target="#modalModificar" onclick="onclick_btnEditar('Producto','${element.id}','${element.nombre}','${element.precio}','${element.descripcion}','','')">
                                         <svg width="1em" height="1em" viewBox="0 0 16 16" class="bi bi-pencil" fill="currentColor" xmlns="http://www.w3.org/2000/svg">
                                             <path fill-rule="evenodd" d="M12.146.146a.5.5 0 0 1 .708 0l3 3a.5.5 0 0 1 0 .708l-10 10a.5.5 0 0 1-.168.11l-5 2a.5.5 0 0 1-.65-.65l2-5a.5.5 0 0 1 .11-.168l10-10zM11.207 2.5L13.5 4.793 14.793 3.5 12.5 1.207 11.207 2.5zm1.586 3L10.5 3.207 4 9.707V10h.5a.5.5 0 0 1 .5.5v.5h.5a.5.5 0 0 1 .5.5v.5h.293l6.5-6.5zm-9.761 5.175l-.106.106-1.528 3.821 3.821-1.528.106-.106A.5.5 0 0 1 5 12.5V12h-.5a.5.5 0 0 1-.5-.5V11h-.5a.5.5 0 0 1-.468-.325z"/>
                                         </svg>
                                     </button>
                                 </td>
-                              <td><button class="btn btn-danger" data-toggle="modal" data-target="#exampleModal" onclick="onclick_btnEliminar()">Eliminar</button></td>
+                              <td><button type="button" onclick="onclick_btnEliminar('Producto')" class="btn btn-danger" data-toggle="modal" data-target="#modalEliminar">Eliminar</button></td>
                             </tr>
                             `;
                         })
@@ -129,13 +130,13 @@
                             <td>${element.id}</td>
                             <td>${element.nombre}</td>
                             <td>
-                                <button class="btn btn-secondary" data-toggle="modal" data-target="#exampleModal" onclick="onclick_btnEditar()">
+                                <button class="btn btn-secondary" data-toggle="modal" data-target="#modalModificar" onclick="onclick_btnEditar('Categoria','${element.id}','${element.nombre}','','','','')">
                                     <svg width="1em" height="1em" viewBox="0 0 16 16" class="bi bi-pencil" fill="currentColor" xmlns="http://www.w3.org/2000/svg">
                                         <path fill-rule="evenodd" d="M12.146.146a.5.5 0 0 1 .708 0l3 3a.5.5 0 0 1 0 .708l-10 10a.5.5 0 0 1-.168.11l-5 2a.5.5 0 0 1-.65-.65l2-5a.5.5 0 0 1 .11-.168l10-10zM11.207 2.5L13.5 4.793 14.793 3.5 12.5 1.207 11.207 2.5zm1.586 3L10.5 3.207 4 9.707V10h.5a.5.5 0 0 1 .5.5v.5h.5a.5.5 0 0 1 .5.5v.5h.293l6.5-6.5zm-9.761 5.175l-.106.106-1.528 3.821 3.821-1.528.106-.106A.5.5 0 0 1 5 12.5V12h-.5a.5.5 0 0 1-.5-.5V11h-.5a.5.5 0 0 1-.468-.325z"/>
                                     </svg>
                                 </button>
                             </td>
-                          <td><button class="btn btn-danger" data-toggle="modal" data-target="#exampleModal" onclick="onclick_btnEliminar()">Eliminar</button></td>
+                            <td><button type="button" onclick="onclick_btnEliminar('Categoría')" class="btn btn-danger" data-toggle="modal" data-target="#modalEliminar">Eliminar</button></td>
                         </tr>
                         `;
                     });
@@ -148,13 +149,13 @@
                             <td>${element.direccion}</td>
                             <td>${element.cuit}</td>
                             <td>
-                                <button class="btn btn-secondary" data-toggle="modal" data-target="#exampleModal" onclick="onclick_btnEditar()">
+                                <button class="btn btn-secondary" data-toggle="modal" data-target="#modalModificar" onclick="onclick_btnEditar('Proveedor','${element.id}','${element.nombre}','','','${element.direccion}','${element.cuit}')">
                                     <svg width="1em" height="1em" viewBox="0 0 16 16" class="bi bi-pencil" fill="currentColor" xmlns="http://www.w3.org/2000/svg">
                                         <path fill-rule="evenodd" d="M12.146.146a.5.5 0 0 1 .708 0l3 3a.5.5 0 0 1 0 .708l-10 10a.5.5 0 0 1-.168.11l-5 2a.5.5 0 0 1-.65-.65l2-5a.5.5 0 0 1 .11-.168l10-10zM11.207 2.5L13.5 4.793 14.793 3.5 12.5 1.207 11.207 2.5zm1.586 3L10.5 3.207 4 9.707V10h.5a.5.5 0 0 1 .5.5v.5h.5a.5.5 0 0 1 .5.5v.5h.293l6.5-6.5zm-9.761 5.175l-.106.106-1.528 3.821 3.821-1.528.106-.106A.5.5 0 0 1 5 12.5V12h-.5a.5.5 0 0 1-.5-.5V11h-.5a.5.5 0 0 1-.468-.325z"/>
                                     </svg>
                                 </button>
                             </td>
-                          <td><button class="btn btn-danger" data-toggle="modal" data-target="#exampleModal" onclick="onclick_btnEliminar()">Eliminar</button></td>
+                            <td><button type="button" onclick="onclick_btnEliminar('Proveedor')" class="btn btn-danger" data-toggle="modal" data-target="#modalEliminar">Eliminar</button></td>
                         </tr>
                         `;
                     });
@@ -164,13 +165,122 @@
             })
         }
         function onclick_btnAgregar(btnAgregar){
-            document.getElementById('tituloModal').innerHTML="Agregar";
+            
         }
-        function onclick_btnEditar(btnAgregar){
-            document.getElementById('tituloModal').innerHTML="Editar";
+        function onclick_btnEditar(tipoABM,id,nombre,precio,descripcion,direccion,cuit){
+            document.getElementById('tituloModificar').innerHTML="Editar "+tipoABM;
+            if (tipoABM=="Producto") {
+                document.getElementById('bodyModificar').innerHTML=` 
+                    <div class="container-fluid">
+                        <div class="row">
+                            <div class="col-6">
+                                <p>Vista previa</p>
+                                <div class="card mx-auto" style="width: 18rem;">
+                                <img src="https://1.bp.blogspot.com/-0V5xiGGwhBc/VK0My5TjBTI/AAAAAAAADxY/cQjkOOq9uqM/s1600/manzana-roja.png" class="card-img-top" alt="...">
+                                <div class="card-body">
+                                        <h2 id="vp_Nombre">${nombre}</h2>
+                                        <h5 id="vp_Precio">$ ${precio}</h5>
+                                        <p  id="vp_Descripcion"class="card-text">${descripcion}</p>
+                                </div>
+                                </div>
+                            </div>
+                            <div class="col-6">
+                                <div class="input-group input-group-sm mb-3">
+                                <div class="input-group-prepend">
+                                    <span class="input-group-text" id="inputGroup-sizing-sm">Nombre</span>
+                                </div>
+                                <input onkeyup="llenarVistaPrevia('vp_Nombre','editarNombre')" id="editarNombre" value="${nombre}" type="text" class="form-control" aria-label="Sizing example input" aria-describedby="inputGroup-sizing-sm">
+                                </div>
+                                <div class="input-group input-group-sm mb-3">
+                                <div class="input-group-prepend">
+                                    <span class="input-group-text" id="inputGroup-sizing-sm">Precio</span>
+                                </div>
+                                <input onkeyup="llenarVistaPrevia('vp_Precio','editarPrecio')" id="editarPrecio" value="${precio}" type="text" class="form-control" aria-label="Sizing example input" aria-describedby="inputGroup-sizing-sm">
+                                </div>
+                                <div class="input-group input-group-sm mb-3">
+                                <div class="input-group-prepend">
+                                    <span class="input-group-text" id="inputGroup-sizing-sm">Descripción</span>
+                                </div>
+                                <input onkeyup="llenarVistaPrevia('vp_Descripcion','editarDescripcion')" id="editarDescripcion" value="${descripcion}" type="text" class="form-control" aria-label="Sizing example input" aria-describedby="inputGroup-sizing-sm">
+                                </div>
+                                <div class="input-group input-group-sm mb-3">
+                                <div class="input-group-prepend">
+                                    <span class="input-group-text" id="inputGroup-sizing-sm">Categoría</span>
+                                </div>
+                                <input type="text" class="form-control" aria-label="Sizing example input" aria-describedby="inputGroup-sizing-sm">
+                                </div>
+                                <div class="input-group input-group-sm mb-3">
+                                <div class="input-group-prepend">
+                                    <span class="input-group-text" id="inputGroup-sizing-sm">Proveedor</span>
+                                </div>
+                                <input type="text" class="form-control" aria-label="Sizing example input" aria-describedby="inputGroup-sizing-sm">
+                                </div>
+                                <div class="input-group">
+                                    <div class="custom-file">
+                                      <input type="file" class="custom-file-input" id="inputGroupFile04" aria-describedby="inputGroupFileAddon04">
+                                      <label class="custom-file-label" for="inputGroupFile04">Imagen</label>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                `;
+            }else if(tipoABM=="Categoria"){
+                document.getElementById('bodyModificar').innerHTML=` 
+                    <div class="container-fluid">
+                        <div class="row">
+                            <div class="col-2"></div>
+                            <div class="col-8">
+                                <div class="input-group input-group-sm mb-3">
+                                <div class="input-group-prepend">
+                                    <span class="input-group-text" id="inputGroup-sizing-sm">Nombre</span>
+                                </div>
+                                <input onkeyup="llenarVistaPrevia('vp_Nombre','editarNombre')" id="editarNombre" value="${nombre}" type="text" class="form-control" aria-label="Sizing example input" aria-describedby="inputGroup-sizing-sm">
+                                </div>
+                            </div>
+                            <div class="col-2"></div>
+                        </div>
+                    </div>
+                `;
+            }else if(tipoABM=="Proveedor"){
+                document.getElementById('bodyModificar').innerHTML=` 
+                    <div class="container-fluid">
+                        <div class="row">
+                            <div class="col-2"></div>
+                            <div class="col-8">
+                                <div class="input-group input-group-sm mb-3">
+                                    <div class="input-group-prepend">
+                                        <span class="input-group-text" id="inputGroup-sizing-sm">Nombre</span>
+                                    </div>
+                                    <input onkeyup="llenarVistaPrevia('vp_Nombre','editarNombre')" id="editarNombre" value="${nombre}" type="text" class="form-control" aria-label="Sizing example input" aria-describedby="inputGroup-sizing-sm">
+                                </div>
+                                <div class="input-group input-group-sm mb-3">
+                                    <div class="input-group-prepend">
+                                        <span class="input-group-text" id="inputGroup-sizing-sm">Dirección</span>
+                                    </div>
+                                    <input onkeyup="llenarVistaPrevia('vp_Nombre','editarNombre')" id="editarNombre" value="${direccion}" type="text" class="form-control" aria-label="Sizing example input" aria-describedby="inputGroup-sizing-sm">
+                                </div>
+                                <div class="input-group input-group-sm mb-3">
+                                    <div class="input-group-prepend">
+                                        <span class="input-group-text" id="inputGroup-sizing-sm">Cuit</span>
+                                    </div>
+                                    <input onkeyup="llenarVistaPrevia('vp_Nombre','editarNombre')" id="editarNombre" value="${cuit}" type="text" class="form-control" aria-label="Sizing example input" aria-describedby="inputGroup-sizing-sm">
+                                </div>
+                            </div>
+                            <div class="col-2"></div>
+                        </div>
+                    </div>
+                `;
+            }
         }
-        function onclick_btnEliminar(btnAgregar){
-            document.getElementById('tituloModal').innerHTML="Eliminar";
+        function llenarVistaPrevia(idALlenar,idDelContenido){
+            document.getElementById(idALlenar).innerHTML=document.getElementById(idDelContenido).value;
+            if (idALlenar=="vp_Precio") {
+                document.getElementById(idALlenar).innerHTML="$ "+document.getElementById(idDelContenido).value;
+            }
+        }
+        function onclick_btnEliminar(tipoABM,id){
+            document.getElementById('tituloModalEliminar').innerHTML="Eliminar "+ tipoABM;
         }
     </script>
 </body>
